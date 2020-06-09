26f2aae2c6e76ac9a1b8da371957403177393bf1
Famosi <simone.faggi@yahoo.it>
2020-06-09 12:26:09 +0200

    add orientations

--- sign.py
+++ sign.py
@@ -15,7 +15,7 @@ class Sign:
     def perform_sign(self):
         Performer("Location", path.path_motions_location, self.robot, "static", self.location, self.l_r)
         Performer("Configuration", path.path_motions_configuration, self.robot, "static", self.configuration, self.l_r)
-        # Module("Orientation", path.path_motions_orientation, self.robot, "static" ,self.configuration, self.l_r)
+        Performer("Orientation", path.path_motions_orientation, self.robot, "static", self.orientation, self.l_r)
         Performer("Movement", path.path_motions_movement, self.robot, "dynamic", self.movement, self.l_r)
         self.rest_position()
 
