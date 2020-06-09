fd7c4cfc9de4bbfb4ef84945ea50b667f50af98d
Famosi <simone.faggi@yahoo.it>
2020-06-09 10:48:47 +0200

    rm Utilities for modules

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-
 """
 Paths Declaration
 """
@@ -9,35 +8,3 @@ path_motions_movement = local_path +'SocialRobot-ISL/motions/movement/'
 path_motions_configuration = local_path +'SocialRobot-ISL/motions/configuration/'
 path_dictionary = local_path +'SocialRobot-ISL/sign_dictionary.json'
 
-
-"""
-Utilities for modules
-"""
-
-
-from controller import Robot, Keyboard, Motion
-
-
-def get_motions(path, l_r, module):
-    if l_r == "L_R":
-        return [
-            Motion(path + module[0] + '_R' + '.motion'),
-            Motion(path + module[1] + '_L' + '.motion')
-        ]
-    else:
-        return [Motion(path + module[0] + '_' + l_r + '.motion')]
-
-
-def perform_module(motion, l_r, robot):
-    if l_r == "L_R":
-        motion[0].play()
-        while not motion[0].isOver():
-            motion[1].play()
-            while not motion[1].isOver():
-                motion[1].play()
-                robot.step(robot.timeStep)
-            robot.step(robot.timeStep)
-    else:
-        motion[0].play()
-        while not motion[0].isOver():
-            robot.step(robot.timeStep)
